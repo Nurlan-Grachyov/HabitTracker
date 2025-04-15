f0d417f79ebfaa4c78d466f6d5ca45f28ae57768
Nurlan-Grachyov <nurlan.grachyov@mail.ru>
2025-04-15 17:55:22 +0400

    add celery and celery beat, the problem with start server

--- habits/services.py
+++ habits/services.py
@@ -1,6 +1,11 @@
+from datetime import timedelta
+import pytz
 import requests
+from django.utils import timezone
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from config.settings import TG_URL, BOT_TOKEN
+from habits.models import Habits
 
 
 def send_tg_message(message, tg_id):
@@ -9,3 +14,31 @@ def send_tg_message(message, tg_id):
         'chat_id': tg_id,
     }
     requests.get(f'{TG_URL}{BOT_TOKEN}/sendMessage', params=params)
+
+
+def is_time_to_send_reminder(habit):
+    tolerance_minutes = 5
+
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    now = timezone.now().astimezone(moscow_tz)
+    start_time_moscow = habit.start_time.astimezone(moscow_tz)
+
+    delta = abs(now - start_time_moscow)
+
+    return delta <= timedelta(minutes=tolerance_minutes)
+
+
+def time_send_message_about_habit():
+    habits = Habits.objects.all()
+    for habit in habits:
+        if is_time_to_send_reminder(habit):
+            schedule, _ = IntervalSchedule.objects.get_or_create(
+                every=habit.periodicity,
+                period=IntervalSchedule.DAYS,
+            )
+
+            PeriodicTask.objects.create(
+                interval=schedule,
+                name=f'Send a reminder about {habit.name}',
+                task='habits.tasks.time_habit',
+            )
--- habits/views.py
+++ habits/views.py
@@ -1,15 +1,9 @@
-import json
-from datetime import timedelta, datetime
-
-from django.utils import timezone
-from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from habits.models import Habits
 from habits.permission import Owner, ListHabits
 from habits.serializers import HabitsSerializer
-from habits.tasks import time_habit
 
 
 class HabitsViewSet(ModelViewSet):
@@ -45,23 +39,3 @@ class HabitsViewSet(ModelViewSet):
             return Habits.objects.filter(owner=self.request.user) or Habits.objects.filter(is_public=True)
         elif self.request.user.is_superuser:
             return Habits.objects.all()
-
-    def time_send_message_about_habit(self):
-        habits = Habits.objects.all()
-        for habit in habits:
-            schedule, created = IntervalSchedule.objects.get_or_create(
-                every=5,
-                period=IntervalSchedule.SECONDS,
-            )
-
-            PeriodicTask.objects.create(
-                interval=schedule,
-                name='Send a reminder about habit',
-                task='habits.tasks.time_habit',
-                args=json.dumps(['arg1', 'arg2']),
-                kwargs=json.dumps({
-                    'be_careful': True,
-                }),
-                expires=timezone.now() + timedelta(seconds=30)
-            )
-            time_habit.delay(maessage=f"Вам пора {habit.action} в {habit.place}",chat_id=habit.owner.tg_id)
