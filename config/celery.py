03945a05d2bc412c8b2e3ef74dcaa5adde51a8eb
Nurlan-Grachyov <nurlan.grachyov@mail.ru>
2025-04-17 09:31:39 +0400

    The task was not started

--- config/celery.py
+++ config/celery.py
@@ -1,15 +1,15 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Установка переменной окружения для настроек проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Создание экземпляра объекта Celery
 app = Celery('config')
 
-# Загрузка настроек из файла Django
+app.conf.enable_utc = False
+app.conf.timezone = 'Europe/Moscow'
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {}
